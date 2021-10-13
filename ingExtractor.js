e8ca295ecb113926e42894ba87258ddbe89c78eb
TimothyAL <timothy_lusby@hotmail.com>
2021-10-13 12:37:01 -0700

    debuging js

--- ingExtractor.js
+++ ingExtractor.js
@@ -1,9 +1,13 @@
 const ingExtractor = (phrase) => {
     const phraseList = phrase.split(' ')
     const result = []
-    for (let i=0; i < length(phraseList); i++) {
-        if (phraseList[i].substring(-3, 0) === 'ing' && length(phraseList[i]) > 4){
-            result.append(phraseList[i])
+    for (let i=0; i < phraseList.length; i++) {
+        if (phraseList[i].substr(-3) === 'ing' && phraseList[i].length > 4){
+            result.push(phraseList[i])
         }
     }
+    return(result)
 }
+
+let phrase = process.argv.slice(2).join(' ')
+console.log(ingExtractor(phrase))
--- vendingMachine.js
+++ vendingMachine.js
@@ -1,10 +1,23 @@
+const products = [
+    {'name':'cookie', 'cost': 50},
+    {'name':'chips', 'cost': 20},
+    {'name':'brownie', 'cost': 100},
+    {'name':'soda', 'cost': 100},
+    {'name':'apple', 'cost': 200},
+    {'name':'pizza', 'cost': 250},
+    {'name':'cake', 'cost': 520}
+]
+
 const vendingMachine = (products, amount, id) => {
     try {
-        const item = products[id]
-        if (item['cost'] > amount) {
+        const item = products[Number(id)]
+        if (typeof item === 'undefined'){
+            throw new RangeError()
+        }
+        if (item['cost'] > Number(amount)) {
             return('Not enough money for this product')
         }
-        let pending_change = item['cost'] - amount
+        let pending_change = Number(amount) - item['cost']
         let change = []
         while (pending_change > 0) {
             if (pending_change >= 500){
@@ -36,5 +49,9 @@ const vendingMachine = (products, amount, id) => {
         if (err instanceof RangeError) {
             return('Enter a valid product number')
         }
+        else{return err}
     }
 }
+
+let result = vendingMachine(products, process.argv[2], process.argv[3])
+console.log(result)
